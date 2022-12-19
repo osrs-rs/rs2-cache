c19ff5b62c5053cb7cd8daa4b3a64123daa70b25
Sage <94207840+Sages0ft@users.noreply.github.com>
2022-12-19 18:22:29 +0100

    Resolved memory violation

--- python/Test.py
+++ python/Test.py
@@ -1,9 +1,10 @@
-import ctypes
+from ctypes import *
 
 
 class Cache:
     # Initialize the Cache object with the loaded library and the cache
     def __init__(self, lib, cache_c):
+        # TODO: Init all the stuff here with loading library and setting relevant types and args instead of in "open"
         self.lib = lib
         self.cache_c = cache_c
 
@@ -11,9 +12,10 @@ class Cache:
     @staticmethod
     def open(path):
         # Load the osrscache library into C types
-        lib = ctypes.cdll.LoadLibrary('../rust/target/debug/osrscache.dll')
+        lib = cdll.LoadLibrary('../rust/target/debug/osrscache.dll')
 
         # Open the Cache using C types
+        lib.cache_open.restype = c_void_p
         cache_c = lib.cache_open(path.encode('utf-8'))
 
         # Return a Cache object, containing the loaded library and the cache
@@ -21,12 +23,18 @@ class Cache:
 
     # Read a file from the cache
     def read(self, archive, group, file):
-        len = 0
+        output_len = c_uint32(0)
+        self.lib.cache_read.argtypes = [
+            c_void_p, c_int, c_int, c_int, c_int, POINTER(c_uint32)]
         self.lib.cache_read(self.cache_c, archive, group,
-                            file, 0, len)
-        print(len)
+                            file, 0, byref(output_len))
+        print(output_len)
 
 
 cache = Cache.open("./cache")
 # Read a blue partyhat from the cache
 cache.read(2, 10, 1042)
+
+
+# Example of python ctypes here:
+# https://stackoverflow.com/questions/26363641/passing-a-pointer-value-to-a-c-function-from-python
