35aa476e47a72efe1294cc6c2761d1a930ce0285
JayArrowz <arrowzflame321@gmail.com>
2023-01-06 11:38:38 +0000

    chore: remove redundant code

--- csharp/OsrsCache/OsrsCacheService.cs
+++ csharp/OsrsCache/OsrsCacheService.cs
@@ -16,38 +16,21 @@ namespace OsrsCache
 
         public byte[] Read(ushort archive, ushort group, ushort file, int[] xtea_keys_param = null)
         {
-            // Start with xtea_keys default to a nullptr
-            var xtea_keys = UIntPtr.Zero;
-            if (xtea_keys_param != null)
-            {
-                // Create pointer to the data and pass it to cache_read
-                // ...
-            }
-
             // Call cache_read
             var out_len = 0;
-            var buf = Internals.cache_read(_cache, archive, group, file, xtea_keys, ref out_len);
+            var buf = Internals.cache_read(_cache, archive, group, file, xtea_keys: UIntPtr.Zero, ref out_len);
 
             // Copy the data from the IntPtr to the byte array
             byte[] managedArray = new byte[out_len];
             Marshal.Copy(buf, managedArray, 0, out_len);
-
             return managedArray;
         }
 
         public unsafe Stream ReadStream(ushort archive, ushort group, ushort file, int[] xtea_keys_param = null)
         {
-            // Start with xtea_keys default to a nullptr
-            var xtea_keys = UIntPtr.Zero;
-            if (xtea_keys_param != null)
-            {
-                // Create pointer to the data and pass it to cache_read
-                // ...
-            }
-
             // Call cache_read
             var out_len = 0;
-            var buf = Internals.cache_read(_cache, archive, group, file, xtea_keys, ref out_len);
+            var buf = Internals.cache_read(_cache, archive, group, file, xtea_keys: UIntPtr.Zero, ref out_len);
             return new UnmanagedMemoryStream((byte*)buf.ToPointer(), out_len);
         }
 
