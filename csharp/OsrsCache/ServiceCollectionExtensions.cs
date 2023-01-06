8c598b8bb65f544b8112230ab0320db2b7b0fad1
JayArrowz <arrowzflame321@gmail.com>
2023-01-06 11:41:55 +0000

    feat: add DI registration methods

---
+++ csharp/OsrsCache/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OsrsCache
+{
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds Osrs cache service to DI
+        /// </summary>
+        /// <param name="serviceProvider">The service provider</param>
+        /// <param name="cachePath">Path to the cache</param>
+        /// <returns></returns>
+        public static IServiceCollection AddOsrsCacheService(this IServiceCollection serviceProvider, string cachePath)
+        {
+            serviceProvider.AddSingleton(new OsrsCacheService(cachePath));
+            return serviceProvider;
+        }
+
+    }
+}
